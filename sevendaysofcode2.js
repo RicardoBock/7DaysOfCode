121856d595aa19e6d7b1324db1a0c14391d4e696
Ricardo Bock Camargo <ricardobockcamargo@gmail.com>
2023-04-20 14:14:52 -0300

    Adicionando o desafio extra ao código

--- sevendaysofcode2.js
+++ sevendaysofcode2.js
@@ -4,3 +4,12 @@ const linguagem = prompt("Qual linguagem de programação você esta estudando?"
 const msg=`"Olá! ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`;
 
 alert(msg);
+
+const gosta = prompt(`"Você esta gostando de estudar ${linguagem}? Responda com o número 1 para SIM e com o número 2 para não"`);
+if (gosta==1)
+{
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+} else 
+{
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+};

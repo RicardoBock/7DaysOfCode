ec20c7f560b8e912290fa575b87d363a2287a2df
Ricardo Bock Camargo <ricardobockcamargo@gmail.com>
2023-04-20 14:09:11 -0300

    Dia 2 de desafios do 7 days of code

---
+++ sevendaysofcode2.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt ("Qual a sua idade?");
+const linguagem = prompt("Qual linguagem de programação você esta estudando?")
+const msg=`"Olá! ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"`;
+
+alert(msg);

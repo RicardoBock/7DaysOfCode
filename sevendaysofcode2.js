a0cc1d39348cfa29d66d77ffa348f2e814bb96ef
Ricardo Bock Camargo <65081990+RicardoBock@users.noreply.github.com>
2023-04-23 15:23:30 -0300

    Adicionando a opção 'indisponível'

--- sevendaysofcode2.js
+++ sevendaysofcode2.js
@@ -9,7 +9,12 @@ const gosta = prompt(`"Você esta gostando de estudar ${linguagem}? Responda com
 if (gosta==1)
 {
     alert("Muito bom! Continue estudando e você terá muito sucesso.");
-} else 
+}
+else if (gosta==2)
 {
     alert("Ahh que pena... Já tentou aprender outras linguagens?");
-};
+}
+else
+{
+    alert("Opção indisponível!")
+}
